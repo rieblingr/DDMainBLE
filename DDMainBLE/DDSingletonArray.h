8c00f0a5bc6c387657ea854b2b6ddd35587b8d6a
Kenneth S <kksiu@wisc.edu>
2014-04-27 17:29:31 -0500

    Added singleton

---
+++ DDMainBLE/DDSingletonArray.h
@@ -0,0 +1,13 @@
+//
+//  DDSingletonArray.h
+//  DDMainBLE
+//
+//  Created by Kenneth Siu on 4/27/14.
+//  Copyright (c) 2014 Ryan Riebling. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DDSingletonArray : NSObject
+
+@end
---
+++ DDMainBLE/DDSingletonArray.m
@@ -0,0 +1,13 @@
+//
+//  DDSingletonArray.m
+//  DDMainBLE
+//
+//  Created by Kenneth Siu on 4/27/14.
+//  Copyright (c) 2014 Ryan Riebling. All rights reserved.
+//
+
+#import "DDSingletonArray.h"
+
+@implementation DDSingletonArray
+
+@end
